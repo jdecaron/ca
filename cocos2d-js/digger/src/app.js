21b7c5b68bc7cf1c1fad84f271a10c697bcaf547
unknown <jdeniscaron@POSTE-0597.frima.local>
2015-05-26 17:04:07 -0400

    Cocos2d-js: Updating the pysicis and the position of the elements.

--- cocos2d-js/digger/src/app.js
+++ cocos2d-js/digger/src/app.js
@@ -8,28 +8,30 @@ var DiggerScene = cc.Scene.extend({
     initPhysics:function() {
         this.space = new cp.Space();
         // Gravity
-        this.space.gravity = cp.v(0, -350);
-        // set up Walls
-        var wallBottom = new cp.SegmentShape(this.space.staticBody,
-            cp.v(0, 0),// start point
-            cp.v(4294967295, 0),// MAX INT:4294967295
-            0);// thickness of wall
-        this.space.addStaticShape(wallBottom);
+        this.space.gravity = cp.v(0, -950);
     },
     onEnter:function () {
         this._super();
         this.initPhysics();
 
-        this.gameLayer = new cc.Layer();
+        this.gameLayer = new cc.LayerColor(cc.color(65,198,255));
+        this.gameLayer.setContentSize(cc.size(120*48, 4000));
 
-        this.player = this.createPhysicsSprite(cc.p(200,200))
-        this.player.body.v_limit = 500;
-        this.gameLayer.addChild(this.player);
-        cc.log('onEnter');
+        /*
+        // Debug physics.
+        this._debugNode = new cc.PhysicsDebugNode(this.space );
+        this._debugNode.visible = true ;
+        this.gameLayer.addChild( this._debugNode );
+        */
 
-        //add three layer in the right order
         this.addChild(this.gameLayer);
         this.scheduleUpdate();
+
+        this.loadLevel();
+
+        this.player = this.createDynamicSprite(cc.p(200,2000), res.player_png)
+        this.player.body.v_limit = 500;
+        this.gameLayer.addChild(this.player);
 
         var self = this;
         if ('keyboard' in cc.sys.capabilities) {
@@ -40,14 +42,24 @@ var DiggerScene = cc.Scene.extend({
                         if(self.player.body.arbiterList == null) { return false; }
                         return true;
                     }
-                    if(isTouchingGround() && key == 65) {
-                        self.player.body.applyImpulse(cp.v(-500, 0), cp.v(0, 0));
-                    } else if(isTouchingGround() && key == 68) {
-                        self.player.body.applyImpulse(cp.v(500, 0), cp.v(0, 0));
-                    } else if(isTouchingGround() && key == 32) {
-                        self.player.body.applyImpulse(cp.v(0, 500), cp.v(0, 0));
+                    if(key == 65) {
+                        if(isTouchingGround()) {
+                            self.player.body.applyForce(cp.v(-1000, 0), cp.v(0, 0));
+                        }else {
+                            self.player.body.applyForce(cp.v(-50, 0), cp.v(0, 0));
+                        }
+                    } else if(key == 68) {
+                        if(isTouchingGround()) {
+                            self.player.body.applyForce(cp.v(1000, 0), cp.v(0, 0));
+                        }else {
+                            self.player.body.applyForce(cp.v(50, 0), cp.v(0, 0));
+                        }
+                    } else if(/*isTouchingGround() && */key == 32) {
+                        self.player.body.applyImpulse(cp.v(0, 750), cp.v(0, 0));
                     }
-                    cc.log(self.player.body);
+                },
+                onKeyReleased: function (key, event) {
+                    self.player.body.f = cp.v(0,0);
                 }
             }, this);
         } else {
@@ -64,19 +76,151 @@ var DiggerScene = cc.Scene.extend({
 
         this.gameLayer.setPosition(cc.p(-eyeX,0));
         */
+        this.gameLayer.setPosition(cc.p(-this.player.getPositionX()+200,-this.player.getPositionY()+200));
     },
-    createPhysicsSprite : function( pos ) {
-        var body = new cp.Body(1, cp.momentForBox(1, 48, 108) );
+    createDynamicSprite : function(pos, file) {
+        var body = new cp.Body(1, cp.momentForBox(1, 48, 48) );
         body.setPos( pos );
         this.space.addBody( body );
-        var shape = new cp.BoxShape( body, 48, 108);
-        shape.setElasticity( 0.5 );
-        shape.setFriction( 0.5 );
+        var shape = new cp.BoxShape( body, 48, 48);
+        shape.setElasticity(0);
+        shape.setFriction(0.5);
         this.space.addShape( shape );
 
-        var sprite = new cc.PhysicsSprite(res.player_png);
+        var sprite = new cc.PhysicsSprite(file);
         sprite.setBody( body );
         return sprite;
     },
+
+    createStaticSprite : function(pos, file) {
+        var body = new cp.Body(Infinity, cp.momentForBox(1, 48, 48) );
+        body.setPos( pos );
+        var shape = new cp.BoxShape( body, 48, 48);
+        shape.setElasticity(0);
+        shape.setFriction(0.9);
+        this.space.addShape( shape );
+        var sprite = new cc.Sprite(file);
+        sprite.setPosition( pos );
+        return sprite;
+    },
+
+    loadLevel: function() {
+        var collision = [];
+        var levelSize = {'x':120, 'y':40};
+        for(var i = 0; i < levelSize.y; i++) {
+            var inside = [];
+            for(var j = 0; j < levelSize.x; j++) {
+                inside.push(0);
+            }
+            collision[i] = inside;
+        }
+        var data = _.clone(collision, true);
+
+        offset = Math.floor((Math.random() * 10000));
+        scale = 20;
+        for(var x = 0; x < levelSize.x; x++) {
+            var value = noise.perlin3((x+offset) / scale, (x+offset) / scale, 0);
+            value = (1 + value) * 1.1 * 10;
+            var y = Math.ceil(value);
+            collision[y][x] = 1;
+            data[y][x] = 1;
+
+            for(var c = y+1; c < collision.length; c++) {
+                collision[c][x] = 1;
+                data[c][x] = 2;
+            }
+
+        }
+        this.original = _.clone(collision, true);
+
+        var offset = 0;
+        var scale = 5;
+        var floodFill = function(cave, limits, x, y) {
+            var key = x + ':' + y;
+            var value = noise.perlin2((x+offset) / scale, (y+offset) / scale);
+            value = Math.floor(value * 256);
+
+            limits.maximum.x = Math.max(limits.maximum.x, x);
+            limits.maximum.y = Math.max(limits.maximum.y, y);
+            limits.minimum.x = Math.min(limits.minimum.x, x);
+            limits.minimum.y = Math.min(limits.minimum.y, y);
+
+            if(_.contains(cave, key)) {
+                return;
+            }
+            if(value <= 0) {
+                return;
+            }
+            cave.push(key);
+            floodFill(cave, limits, x-1, y);
+            floodFill(cave, limits, x+1, y);
+            floodFill(cave, limits, x, y-1);
+            floodFill(cave, limits, x, y+1);
+            return;
+        }
+
+        var caveList = [];
+        var limitList = [];
+        var generateCaves = function() {
+            var cave = [];
+            var limits = {'minimum': {'x':Infinity, 'y':Infinity}, 'maximum': {'x': -Infinity, 'y': -Infinity}};
+            floodFill(cave, limits, Math.floor((Math.random() * 100)), Math.floor((Math.random() * 100)));
+            if(cave.length > 0) {
+                caveList.push(cave);
+                limitList.push(limits);
+            }
+        }
+        for(var i = 0; i < 20; i++) {
+            generateCaves();
+        }
+
+        if(caveList.length > 0) {
+            var firstBlock = Number(caveList[0][0].split(':')[1]);
+            for(var c = 0; c < caveList[0].length; c++) {
+                // Creating caves from the list.
+                var key = caveList[0][c].split(':');
+                key[0] = Number(key[0]-limitList[0].minimum.x);
+                key[1] = Math.min(Math.max(Number(key[1]-limitList[0].minimum.y), 0), levelSize.y-1);
+                collision[key[1]][key[0]] = 0;
+                if(data[key[1]][key[0]]) {
+                    data[key[1]][key[0]] = 3;
+                }
+            }
+        }
+
+        for(var i = 0; i < data.length; i++) {
+            for(var j = 0; j < data.length; j++) {
+                if(data[i][j] == 1) {
+                    this.gameLayer.addChild(this.createStaticSprite(cc.p(j*48,-(i*48)+2000), res.green_cube_png));
+                }
+                else if(data[i][j] == 2) {
+                    var checkSurrounding = function() {
+                        if(i == 0 || j == 0 || i >= data.length-1 || j >= data[i].length-1 || data[i-1][j] == 0 || data[i][j-1] == 0 || data[i+1][j] == 0 || data[i][j+1] == 0
+                            || data[i-1][j] == 3 || data[i][j-1] == 3 || data[i+1][j] == 3 || data[i][j+1] == 3
+                        ) {
+                            return 'empty';
+                        } else {
+                            return 'block';
+                        }
+                    };
+                    if(checkSurrounding() == 'empty') {
+                        this.gameLayer.addChild(this.createStaticSprite(cc.p(j*48,-(i*48)+2000), res.dirt_cube_png));
+                    }
+                    else {
+                        var sprite = new cc.Sprite();
+                        sprite.initWithFile(res.dirt_cube_png,cc.rect(0,0,48,48));
+                        sprite.setPosition(cc.p(j*48,-(i*48)+2000));
+                        this.gameLayer.addChild(sprite);
+                    }
+                }
+                else if(data[i][j] == 3) {
+                    var sprite = new cc.Sprite();
+                    sprite.initWithFile(res.background_cube_png,cc.rect(0,0,48,48));
+                    sprite.setPosition(cc.p(j*48,-(i*48)+2000));
+                    this.gameLayer.addChild(sprite);
+                }
+            }
+        }
+    }
 });
 
--- cocos2d-js/digger/src/resource.js
+++ cocos2d-js/digger/src/resource.js
@@ -1,4 +1,7 @@
 var res = {
+    background_cube_png : "res/background_cube.png",
+    dirt_cube_png : "res/dirt_cube.png",
+    green_cube_png : "res/green_cube.png",
     player_png : "res/player.png"
 };
 
