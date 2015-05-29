0616e062fc389f80a814530d18280fb40c9b9b6e
unknown <jdeniscaron@POSTE-0597.frima.local>
2015-05-29 10:49:26 -0400

    Cocos2D-js: Removing the folder.

--- cocos2d-js/game/src/app.js
+++ cocos2d-js/game/src/app.js
@@ -1,78 +1,49 @@
 
-var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
-        //////////////////////////////
-        // 1. super init first
+
+var MenuLayer = cc.Layer.extend({
+    ctor : function(){
+        //1. call super class's ctor function
+        this._super();
+    },
+    init:function(){
+        //call super class's super function
         this._super();
 
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
-        var size = cc.winSize;
+        //2. get the screen size of your game canvas
+        var winsize = cc.director.getWinSize();
 
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = new cc.MenuItemImage(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
+        //3. calculate the center point
+        var centerpos = cc.p(winsize.width / 2, winsize.height / 2);
 
-        var menu = new cc.Menu(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
+        //4. create a background image and set it's position at the center of the screen
+        var spritebg = new cc.Sprite(res.helloBG_png);
+        spritebg.setPosition(centerpos);
+        this.addChild(spritebg);
 
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
-        var helloLabel = new cc.LabelTTF("Hello World", "Arial", 38);
-        // position the label on the center of the screen
-        helloLabel.x = size.width / 2;
-        helloLabel.y = 0;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+        //5.
+        cc.MenuItemFont.setFontSize(60);
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite(res.HelloWorld_png);
-        this.sprite.attr({
-            x: size.width / 2,
-            y: size.height / 2,
-            scale: 0.5,
-            rotation: 180
-        });
-        this.addChild(this.sprite, 0);
+        //6.create a menu and assign onPlay event callback to it
+        var menuItemPlay= new cc.MenuItemSprite(
+    		new cc.Sprite(res.start_n_png), // normal state image
+    		new cc.Sprite(res.start_s_png), //select state image
+            this.onPlay, this);
+        var menu = new cc.Menu(menuItemPlay);  //7. create the menu
+        menu.setPosition(centerpos);
+        this.addChild(menu);
+    },
 
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        helloLabel.runAction(
-            cc.spawn(
-                cc.moveBy(2.5, cc.p(0, size.height - 40)),
-                cc.tintTo(2.5,255,125,0)
-            )
-        );
-        return true;
+    onPlay : function(){
+        cc.log("==onplay clicked");
+        cc.director.runScene(new PlayScene());
     }
 });
 
-var HelloWorldScene = cc.Scene.extend({
+var MenuScene = cc.Scene.extend({
     onEnter:function () {
         this._super();
-        var layer = new HelloWorldLayer();
+        var layer = new MenuLayer();
+        layer.init();
         this.addChild(layer);
     }
 });
-
