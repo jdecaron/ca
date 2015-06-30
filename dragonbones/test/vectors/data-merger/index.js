cef2149036d28051753c27ec644279affa13e2ce
unknown <jdeniscaron@POSTE-0597.frima.local>
2015-06-30 15:26:07 -0400

    DragonBones: Adding vector atlas into DragonBones. 	modified:   data-merger/index.js 	modified:   data-merger/texture.json 	modified:   index.htm 	modified:   texture.json

--- dragonbones/test/vectors/data-merger/index.js
+++ dragonbones/test/vectors/data-merger/index.js
@@ -9,15 +9,14 @@ var usageError = function() {
 }
 
 if(args.length != 2) {
-    console.log(args);
     usageError();
 }
 
-if(args[0].match(/.js$/) == null) {
+if(args[0].match(/.json$/) == null) {
     usageError();
 }
 
-if(args[1].match(/.json$/) == null) {
+if(args[1].match(/.js$/) == null) {
     usageError();
 }
 
@@ -25,11 +24,10 @@ var createjs = {};
 createjs.Container = function() {};
 createjs.MovieClip = function() {};
 createjs.Rectangle = function() {};
-
-eval(fs.readFileSync(args[0], 'utf8'));
-
 var encoding = 'utf8';
-var texture = JSON.parse(fs.readFileSync(args[1], encoding));
+
+eval(fs.readFileSync(args[1], encoding));
+var texture = JSON.parse(fs.readFileSync(args[0], encoding));
 
 var removeQuotes = function(properties) {
     for(var i=0; i<properties.length; i++) {
@@ -48,10 +46,12 @@ for(var i=0; i<texture.SubTexture.length; i++) {
         var properties = line.match(/"[^\)]*/g);
         if(properties) {
             removeQuotes(properties);
+            var transform = lines[j+1].match(/\(.*\)/)[0].replace(/[\(\)]/g, '').split(',');
+            properties.push(transform);
             objectList.push(properties);
         }
     }
-    object.shapes = objectList;
+    object.shapes = objectList.reverse();
 }
 
-fs.writeFileSync(args[1], JSON.stringify(texture), encoding);
+fs.writeFileSync(args[0], JSON.stringify(texture), encoding);
